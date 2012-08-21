222f69d571558201c2d230b03b1441381d5cbd5a
Sviatoslav Chumakov <svchumakov@gmail.com>
2012-08-21 11:06:43 +0300

    renamed directories

--- 2/2.hs
+++
@@ -1,6 +0,0 @@
-fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-fibs2 = [x | x <- takeWhile (<4000000) fibs, even x]
-
-main = putStrLn $ show (sum fibs2)
-
--- 5/5.hs
+++
@@ -1,3 +0,0 @@
-evenlyDivisible = [ x | x <- [20, 40 ..], x `mod` 2 == 0 && x `mod` 3 == 0 && x `mod` 4 == 0 && x `mod` 5 == 0 && x `mod` 6 == 0 && x `mod` 7 == 0 && x `mod` 8 == 0 && x `mod` 9 == 0 && x `mod` 10 == 0 && x `mod` 11 == 0 && x `mod` 12 == 0 && x `mod` 13 == 0 && x `mod` 14 == 0 && x `mod` 15 == 0 && x `mod` 16 == 0 && x `mod` 17 == 0 && x `mod` 18 == 0 && x `mod` 19 == 0 && x `mod` 20 == 0]
-
-main = putStrLn $ show (head evenlyDivisible)  
