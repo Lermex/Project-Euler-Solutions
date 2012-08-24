e2955997154d5fbdac449d127b9d1e1ba8683482
Sviatoslav <lermex@localhost.localdomain>
2012-08-25 00:05:14 +0300

    changed formatting

--- assembly-mips/001/001.asm
+++ assembly-mips/001/001.asm
@@ -3,29 +3,29 @@
 # by Sviatoslav "Lermex" Chumakov
 
 .text
-		li		$t1, 0			# Loop counter
-		li		$t8, 0			# Sum of integers - answer
+    li      $t1,0			# Loop counter
+    li      $t8,0			# Sum of integers - answer
 loop:		
-		addi	$t1,$t1,1
-		li   	$t3, 3
-		div 	$t1,$t3
-		mfhi	$t4
-		beqz	$t4, addNumber
-		li  	$t3, 5
-		div 	$t1, $t3
-		mfhi	$t4
-		beqz	$t4, addNumber
+    addi    $t1,$t1,1
+    li      $t3,3
+    div     $t1,$t3
+    mfhi    $t4
+    beqz    $t4,addNumber
+    li      $t3,5
+    div     $t1,$t3
+    mfhi    $t4
+    beqz    $t4,addNumber
 condition:
-		blt		$t1,999,loop
-		nop
-		j 		exit
+    blt     $t1,999,loop
+    nop
+    j       exit
 
 addNumber:
-		add		$t8,$t8,$t1
-		j 		condition
-		
+    add     $t8,$t8,$t1
+    j       condition
+
 exit:	
-		li		$v0, 1			# v0 = 1 - "Print Integer"
-		move    $a0, $t8
-		syscall
-	
+    li      $v0,1			# v0 = 1 - "Print Integer"
+    move    $a0,$t8
+    syscall
+
